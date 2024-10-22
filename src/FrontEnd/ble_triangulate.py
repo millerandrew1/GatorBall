017f8af369077b2e4b9d6f3925b008a6ae6d75bd
joshwhite17 <103671949+joshwhite17@users.noreply.github.com>
2024-10-22 11:32:18 -0400

    Add files via upload

--- src/FrontEnd/ble_triangulate.py
+++ src/FrontEnd/ble_triangulate.py
@@ -29,27 +29,46 @@ async def connect(address):
         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
         print("Model Number: {0}".format("".join(map(chr, model_number))))
 
-# asyncio.run(connect(address))
-
+#currently this gets user inputs in the place of actual information from anchors since we are limited by not having hardware
+#main information we will need from the anchors would be the distance to the ball since achors will be at fixed locations on the field
+#we would need to break down the field into an xy plane, a simple way to do this would be to treat the values for calculation as
+#the yardage values
+loc = 0 
 def calculate():
     P=lx.Project(mode='2D',solver='LSE') 
+    a_x = input("A x value = ")
+    a_y = input("A x value = ")
+    b_x = input("B x value = ")
+    b_y = input("B y value = ")
+    a_dist= input("A to point = ")
+    b_dist = input("B to points = ")
     
+    a_x = int(a_x)
+    a_y = int(a_y)
+    b_x = int(b_x)
+    b_y = int(b_y)
+    a_dist = int(a_dist)
+    b_dist = int(b_dist)
 
-    P.add_anchor('anchore_A',(0,100))
-    P.add_anchor('anchore_B',(100,100))
-    P.add_anchor('anchore_C',(100,0))
+    P.add_anchor('anchore_A',(a_x,a_y))
+    P.add_anchor('anchore_B',(b_x,b_y))
+    #P.add_anchor('anchore_C',(100,0))
 
     t,label=P.add_target()
 
-    t.add_measure('anchore_A',50)
-    t.add_measure('anchore_B',50)
-    t.add_measure('anchore_C',50)
+    t.add_measure('anchore_A',a_dist)
+    t.add_measure('anchore_B',b_dist)
+    #t.add_measure('anchore_C',50)
 
     P.solve()
 
 # Then the target location is:
 
     print(t.loc)
+    loc = t.loc
 
+#calls to functions, can also connect to a BLE device for demo in pre-alpha
+
+#asyncio.run(connect(address))
 #asyncio.run(find())
 calculate()
