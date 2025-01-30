4a4c194623751bf7557e6287f7bf164a3721205c
Ricardo Nivar-Cortes <rnivarcortes@ufl.edu>
2025-01-30 15:21:15 -0500

    Basic Implementation | Login Page and Placeholder Data Page

---
+++ react/src/Data.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Data() {
+  return (
+    <div>
+      <h2>FOOTBALL FIELD</h2>
+      <Link to="/">Logout</Link>
+    </div>
+  );
+}
+
+export default Data;
